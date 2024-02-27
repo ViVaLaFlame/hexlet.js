3afc799ac4089a3172b58a73a97e99b6e8adb767
ViVaLaFlame <amir.kakharov2001@gmail.com>
2024-02-27 22:11:30 +0300

    all done

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-/* eslint-disable no-console */
-import _ from 'lodash';
+let library = [];
 
-console.log('');
-console.log(_.last(['one', 'two']));
+const addBook = (title, author, year, genre, pageCount) => {
+  const book = {
+    title,
+    author,
+    year,
+    genre,
+    pageCount,
+  };
+  library.push(book);
+};
+
+const removeBook = (title) => {
+  library = library.filter((book) => book.title !== title);
+};
+
+const findBooksByAuthor = (author) => {
+  library.filter((book) => book.author === author);
+};
+
+const filterBooksByGenre = (genre) => {
+  library.filter((book) => book.genre === genre);
+};
+
+const genreReport = () => {
+  const report = {};
+  library.forEach((book) => {
+    if (report[book.genre]) {
+      report[book.genre] += 1;
+    } else {
+      report[book.genre] = 1;
+    }
+  });
+  return report;
+};
+
+const averagePagesReport = () => {
+  const totalBooks = library.length;
+  const totalPageCount = library.reduce((sum, book) => sum + book.pageCount, 0);
+  return totalPageCount / totalBooks;
+};
